90a965c506d40e996671ec021c3ba718a18b90ee
Quetzal Meneghetti <53628265+qmeneghetti@users.noreply.github.com>
2025-03-30 05:31:23 +0000

    update version

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     "name": "Custom Product Feed",
     "summary": "Genera un feed XML para Google Shopping y Facebook",
     "author": "Quetzal Meneghetti",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "depends": ["website", "product"],
     "data": [
         "views/product_template_views.xml",  # Agregamos la vista aquí
