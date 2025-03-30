4325126e0ba8389f07dfccc992f0f8f6cc252879
Quetzal Meneghetti <53628265+qmeneghetti@users.noreply.github.com>
2025-03-30 08:02:31 +0000

    fixed claude

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,13 @@
     "summary": "Genera un feed XML para Google Shopping y Facebook",
     'category': 'Sales',
     "author": "Quetzal Meneghetti",
-    "version": "1.1.7",
+    "version": "1.1.8",
     "depends": ["base","website", "product"],
     "data": [
+        "security/ir.model.access.csv",  # Añadir esta línea
         "views/product_template_views.xml",  
+        "views/feed_views.xml",  # Cambia el orden: primero views, luego menu
         "views/feed_menu.xml",
-        "views/feed_views.xml", 
     ],
     "installable": True,
     "application": True,
